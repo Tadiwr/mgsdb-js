2906300fa18535f6e481a6bc1cabc02c6009f6e5
Tadiwanashe Shangwa <80280920+Tadiwr@users.noreply.github.com>
2023-09-01 17:25:08 +0200

    adds cors
    - adds config to only allow the frontend  to externally communicate with the server during production

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -9,6 +10,15 @@ var apiRouter = require('./routes/api');
 var authRouter = require('./routes/auth');
 
 var app = express();
+
+app.use(cors({
+    // CORS OPTIONS HERE
+
+    // ONLY ALLOWS REQUESTS FROM  PORT 5173 
+    // DURING DEVELOPEMENT
+    "origin": 'http://localhost:5173',
+    "optionsSuccessStatus": 200
+}))
 
 app.use(logger('dev'));
 app.use(express.json());
