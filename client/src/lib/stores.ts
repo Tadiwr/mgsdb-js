6566046362459c2e2fedd67b9a5100b212374834
Tadiwanashe Shangwa <80280920+Tadiwr@users.noreply.github.com>
2023-11-02 11:18:26 +0200

    adds stores

--- client/src/lib/stores.ts
+++ client/src/lib/stores.ts
@@ -1,10 +1,24 @@
 import { writable } from "svelte/store";
+import { Student } from "./api/model/student";
 
 const pathUrls = {
     home : "/",
     addStudent : "/add-student",
     getStudent : "/get-student",
+    studentInfo : "/student-info"
 }
+
+const currentStudent : Student = Student.empty();
+
 
 export default pathUrls;
 export const urlStore = writable(pathUrls.home)
+export const currentStudentStore = writable(currentStudent)
+
+
+export function navigateTo(path : string) {
+    urlStore.update((old) => {
+        console.log(`Used navigation function to navigate to ${path}`)
+        return path;
+    })   
+}
