5666acde641a020021d948bb25c2504283f25c9b
Tadiwanashe Shangwa <80280920+Tadiwr@users.noreply.github.com>
2023-11-02 11:17:56 +0200

    generated code

--- client/src/lib/page/GetStudents.svelte
+++ client/src/lib/page/GetStudents.svelte
@@ -1,3 +1,40 @@
+<script lang="ts" >
+    import { Student } from '../api/model/student';
+    import Result from './result.svelte';
+    import AllStudents from './All_students.svelte';
+
+    $: studentName = '';
+    $: payload = [Student.empty()];
+    $: errMessage = '';
+    $: showResults = false;
+
+</script>
+
 <main>
     <h1>Query Students</h1>
+
+    <p>{errMessage}</p>
+    <input placeholder="Search students by name" bind:value={studentName} />
+    <button >Search</button>
+
+    <h1>Results</h1>
+
+    {#if studentName == ""}
+        <h1>All Students</h1>
+        <AllStudents/>
+    {:else}
+    <Result bind:studentName={studentName} />
+    {/if}
+
+
 </main>
+
+<style>
+    input {
+        background-color: white;
+        border-radius: 10px;
+        height: 30px;
+        width: 50%;
+        color: black;
+    }
+</style>
